6b1413078283f9d27a00834419811cdabbdbb3e1
DeveloperMODE-korea <freexxx9206@naver.com>
2025-09-01 03:37:18 +0900

    fix: Resolve build error and include other modifications

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   env: {
     FRED_API_KEY: process.env.FRED_API_KEY,
   },
-}
+  webpack: (config) => {
+    config.externals.push({
+      '@std/testing/mock': 'mock',
+      '@std/testing/bdd': 'bdd',
+      '@gadicc/fetch-mock-cache/runtimes/deno.ts': 'deno.ts',
+      '@gadicc/fetch-mock-cache/stores/fs.ts': 'fs.ts',
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
